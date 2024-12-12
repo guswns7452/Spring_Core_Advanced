b979a3ee49232804eba516e654d1b1d8e75643fd
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-12-13 01:13:34 +0900

    :wrench: 이전 컴퓨터에서 commit

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -21,9 +21,9 @@ import org.springframework.context.annotation.Import;
 // @Import(DynamicProxyFilterConfig.class)
 // @Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
-//@Import(BeanPostProcessorConfig.class)
 //@Import(AutoProxyConfig.class)
-@Import(AopConfig.class)
+//@Import(AopConfig.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
